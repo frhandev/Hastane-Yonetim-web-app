67579c2e06ac44c3e71a2dc39a2c660e333fcb71
frhandev <frhandev@gmail.com>
2025-03-08 09:31:04 +0300

    Created Speciality Component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,6 @@ import { Navigate } from "react-router-dom";
 const Header = () => {
   return (
     <div className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20">
-      {/* left side */}
       <div className="md:w-1/2 flex flex-col items-start gap-4 m-auto md:py-[10vw] md:mb-[-30px]">
         <p className="text-3xl md:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
           Book Appointment <br /> With Trusted Doctors
@@ -25,7 +24,6 @@ const Header = () => {
           Book appointment <img className="w-3" src={assets.arrow_icon} />
         </a>
       </div>
-      {/* right side */}
       <div className="md:w-1/2 relative">
         <img
           className="w-full md:absolute bottom-0 h-auto rounded-lg"
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/header";
+import SpecialityMenu from "../components/SpecialityMenu";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
     </div>
   );
 };
