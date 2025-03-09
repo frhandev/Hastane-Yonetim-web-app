ec428ad64d76acc0d5b4a3d004a644fb597ca007
frhandev <frhandev@gmail.com>
2025-03-09 13:57:33 +0300

    Created All Doctors Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,23 +14,22 @@ const Navbar = () => {
         onClick={() => Navigate("/")}
         className="w-44 cursor-pointer"
         src={assets.logo}
-        alt=""
       />
       <ul className="hidden md:flex item-start gap-5 font-medium">
         <NavLink to={"/"}>
-          <li className="py-1">Home</li>
+          <li className="py-1">HOME</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to={"/doctors"}>
-          <li className="py-1">All Doctors</li>
+          <li className="py-1">ALL DOCTORS</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to={"/about"}>
-          <li className="py-1">About</li>
+          <li className="py-1">ABOUT</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to={"/contact"}>
-          <li className="py-1">Contact</li>
+          <li className="py-1">CONTACT</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
