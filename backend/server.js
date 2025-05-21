379585ff7d49633b3b07c2a780fc3d0cfb2b68dd
Muhammed Ferhan <frhandev@gmail.com>
2025-05-21 14:15:01 +0300

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv/config";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cors());
