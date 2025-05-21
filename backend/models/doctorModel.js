7a59437bb584a15467df83a1f2c1131913d4586d
Muhammed Ferhan <frhandev@gmail.com>
2025-05-21 15:41:50 +0300

    doctorModel Created

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, uniqe: true },
+    image: { type: String, required: true },
+    speciality: { type: String, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    available: { type: Boolean, required: true },
+    fee: { type: Number, required: true },
+    adress: { type: Object, required: true },
+    data: { type: Number, required: true },
+    slots_booked: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
+
+const doctorModel =
+  mongoose.model.doctor || mongoose.model("doctor", doctorSchema);
+
+export default doctorModel;
