b3f98c9e2ae15d07c4b9be14ea59284ee5e7d77f
frhandev <frhandev@gmail.com>
2025-07-19 20:59:11 +0300

    .

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("database connected!"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`);
+  await mongoose.connect(`${process.env.MONGODB_URI}`);
 };
 
 export default connectDB;
