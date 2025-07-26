1664d3304fa69ea046b98a2212eece5cf784ec42
frhandev <frhandev@gmail.com>
2025-07-26 15:55:46 +0300

    Adding Doctors

--- backend/middlwares/authAdmin.js
+++ backend/middlwares/authAdmin.js
@@ -2,15 +2,15 @@ import jwt from "jsonwebtoken";
 
 const authAdmin = async (req, res, next) => {
   try {
-    const { aToken } = req.headers;
-    if (!aToken) {
+    const { atoken } = req.headers;
+    if (!atoken) {
       return res.json({
         success: false,
         message: "Not Authorized Login Again",
       });
     }
 
-    const token_decode = jwt.verify(aToken, process.env.JWT_SECRET);
+    const token_decode = jwt.verify(atoken, process.env.JWT_SECRET);
     if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
       return res.json({
         success: false,
