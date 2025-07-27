31ffb49e6fdcba5e749f8fd98da270f254709292
frhandev <frhandev@gmail.com>
2025-07-27 08:25:02 +0300

    User Profile Update Backend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,23 @@
 import express from "express";
-import { loginUser, registerUser } from "../controlers/userController.js";
+import {
+  getProfile,
+  loginUser,
+  registerUser,
+  updateProfile,
+} from "../controlers/userController.js";
+import authUser from "../middlwares/authUser.js";
+import upload from "../middlwares/multer.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post(
+  "/update-profile",
+  upload.single("image"),
+  authUser,
+  updateProfile
+);
 
 export default userRouter;
